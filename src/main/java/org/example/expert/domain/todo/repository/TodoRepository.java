69fb5eaffc68681939092cc8e2985ac026d99f67
romin014 <sohyun014@naver.com>
2025-04-19 21:12:41 +0900

    refactor : lv2 N+1 problem

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,7 +12,12 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    // N+1 문제 : 연관 관계가 설정된 엔티티를 조회할 경우 최초 조회 쿼리 1개를 포함하여 연관관계의 추가 조회 쿼리가 데이터 갯수(n)개 만큼 추가로 발생
+    // 해결방법 : Fetch Join, @EntityGraph, @BatchSize, DTO 사용 등
+    //
+    //@Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    @Query("SELECT t FROM Todo t ORDER BY t.modifiedAt DESC")
+    @EntityGraph(attributePaths = {"user"})
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
